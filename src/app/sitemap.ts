629154c90517ab6d17e6c8b06d60195b884045fd
Andrey Murashev <andreymurashev@gmail.com>
2025-09-02 17:45:58 +0300

    Add robots + sitemap

---
+++ src/app/sitemap.ts
@@ -0,0 +1,28 @@
+import type { MetadataRoute } from "next";
+
+import { productPageRoute } from "@/constants/routes";
+
+const DOMAIN = 'https://www.selenique.space/'
+
+// TODO: Do not generate on demand
+export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
+  const pages = [
+    {
+      url: DOMAIN,
+      lastModified: new Date(),
+      changeFrequency: "daily",
+      priority: 1,
+    },
+  ] as MetadataRoute.Sitemap;
+
+  [productPageRoute.getUrl()].forEach(item => {
+    pages.push({
+      url: `${DOMAIN}${item}`,
+      lastModified: new Date(),
+      changeFrequency: "daily",
+      priority: 1,
+    });
+  })
+
+  return pages;
+}
