3daf8d11bac38d759f97efe5463b9758caffc8e8
Andrey Murashev <andreymurashev@gmail.com>
2025-09-03 16:32:57 +0300

    Update header

--- src/components/pages/home/index.tsx
+++ src/components/pages/home/index.tsx
@@ -6,11 +6,13 @@ import { useIntl } from "react-intl";
 import styles from "./page.module.css";
 import Card from "./card";
 import { aiConsultationPageRoute, productPageRoute } from "@/constants/routes";
+import Header from "@/components/sections/header";
 
 export default function HomePage() {
   const { formatMessage } = useIntl();
   return (
     <div className={styles.page}>
+      <Header />
       <main className={styles.main}>
         <h2>Наши услуги</h2>
         <div className={styles.items}>
