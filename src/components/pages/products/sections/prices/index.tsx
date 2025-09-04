a5ba91d457f1e8d186f31e50503a5fd46b8a4a2b
Andrey Murashev <andreymurashev@gmail.com>
2025-09-04 12:51:49 +0300

    Update price value

--- src/components/pages/products/sections/prices/index.tsx
+++ src/components/pages/products/sections/prices/index.tsx
@@ -13,10 +13,7 @@ const CheckMarkIcon = () => (
     viewBox="0 0 1920 1920"
     xmlns="http://www.w3.org/2000/svg"
   >
-    <g
-      strokeLinecap="round"
-      strokeLinejoin="round"
-    ></g>
+    <g strokeLinecap="round" strokeLinejoin="round"></g>
     <g id="SVGRepo_iconCarrier">
       <path
         d="M1743.858 267.012 710.747 1300.124 176.005 765.382 0 941.387l710.747 710.871 1209.24-1209.116z"
@@ -57,20 +54,27 @@ const PRICES = [
 export const Prices: React.FC = () => {
   const { formatMessage } = useIntl();
   return (
-    <Section title={formatMessage({ id: "products.prices.title" })} isGray={false} size="960">
+    <Section
+      title={formatMessage({ id: "products.prices.title" })}
+      isGray={false}
+      size="960"
+    >
+      {/* <p>
+        Бесплатный пробник: делаешь одно фото их товара «в подарок» для теста.
+      </p> */}
       <div className={styles.sections}>
         {PRICES.map((item) => (
           <div key={item.title} className={styles.card}>
             <div className={styles.cardDetails}>
-            <h3 className={styles.title}>
-              {formatMessage({ id: item.title })}
-            </h3>
-            <div
-              dangerouslySetInnerHTML={{
-                __html: formatMessage({ id: item.subtitle }),
-              }}
-              className={styles.subtitle}
-            />
+              <h3 className={styles.title}>
+                {formatMessage({ id: item.title })}
+              </h3>
+              <div
+                dangerouslySetInnerHTML={{
+                  __html: formatMessage({ id: item.subtitle }),
+                }}
+                className={styles.subtitle}
+              />
             </div>
             <p className={styles.price}>{formatMessage({ id: item.price })}</p>
 
