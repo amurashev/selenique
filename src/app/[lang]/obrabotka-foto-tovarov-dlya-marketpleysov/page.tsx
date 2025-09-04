03860fee0c06a0fa098213a6c2f0cf9fd3156025
Andrey Murashev <andreymurashev@gmail.com>
2025-09-04 18:55:00 +0300

    Add openGraph

--- src/app/[lang]/obrabotka-foto-tovarov-dlya-marketpleysov/page.tsx
+++ src/app/[lang]/obrabotka-foto-tovarov-dlya-marketpleysov/page.tsx
@@ -6,7 +6,8 @@ import { Metadata } from "next";
 
 export const metadata: Metadata = {
   title: "Обработка фото товаров для маркетплейсов — lifestyle-композиты",
-  description: "Делаю фотореалистичные изображения товара для Ozon, Wildberries, Яндекс.Маркет. Песок, камень, вода, мрамор. Реалистичные тени и цвет. Готово для карточек и рекламы.",
+  description:
+    "Делаю фотореалистичные изображения товара для Ozon, Wildberries, Яндекс.Маркет. Песок, камень, вода, мрамор. Реалистичные тени и цвет. Готово для карточек и рекламы.",
 };
 
 export default async function ProductPage({
