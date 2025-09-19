e2b95d9bfcfe2409614be6500effb661d7471818
Andrey Murashev <andreymurashev@gmail.com>
2025-09-19 21:02:00 +0300

    Add oG

--- src/app/en/prompt-books/[slug]/page.tsx
+++ src/app/en/prompt-books/[slug]/page.tsx
@@ -1,10 +1,36 @@
 import { notFound } from "next/navigation";
+import type { Metadata } from "next";
 
 import Layout from "@/components/layout";
 
 import PromptbookPage from "@/components/pages/promptbook";
 
 import { PROMTBOOKS } from "@/constants/promptbooks";
+
+export async function generateMetadata({
+  params,
+}: {
+  params: Promise<{ slug: string }>;
+}): Promise<Metadata> {
+  const slug = (await params).slug;
+
+  const data = PROMTBOOKS[slug || "null"];
+
+  let title = data.name;
+  let description = "";
+
+  return {
+    title,
+    description,
+    openGraph: {
+      images: [`https://www.selenique.space${data.images[0]}`],
+      title: title,
+      description,
+      url: `https://www.selenique.space/prompt-books/${slug}`,
+      type: "website",
+    },
+  };
+}
 
 export default async function PromptbookPageEntry({
   params,
