6d5e78599f6c58a21893ce8dcb58001c8ca76b0a
Andrey Murashev <andreymurashev@gmail.com>
2026-01-15 20:12:51 +0300

    Fix TS issue

--- src/utils/arrays.ts
+++ src/utils/arrays.ts
@@ -1,15 +1,17 @@
+// eslint-disable-line @typescript-eslint/no-explicit-any
 export default function shuffle(array: any[]) {
   let currentIndex = array.length;
 
   // While there remain elements to shuffle...
   while (currentIndex != 0) {
-
     // Pick a remaining element...
-    let randomIndex = Math.floor(Math.random() * currentIndex);
+    const randomIndex = Math.floor(Math.random() * currentIndex);
     currentIndex--;
 
     // And swap it with the current element.
     [array[currentIndex], array[randomIndex]] = [
-      array[randomIndex], array[currentIndex]];
+      array[randomIndex],
+      array[currentIndex],
+    ];
   }
 }
