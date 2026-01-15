355a64d983bd78d1fcd89c2c33a4590275247f4f
Andrey Murashev <andreymurashev@gmail.com>
2026-01-15 20:14:22 +0300

    Fix TS issue

--- src/utils/arrays.ts
+++ src/utils/arrays.ts
@@ -1,4 +1,4 @@
-// eslint-disable-line @typescript-eslint/no-explicit-any
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
 export default function shuffle(array: any[]) {
   let currentIndex = array.length;
 
