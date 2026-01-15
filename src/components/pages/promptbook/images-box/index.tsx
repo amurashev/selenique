a5b7d303416110c6650cd453d1030561374ac535
Andrey Murashev <andreymurashev@gmail.com>
2026-01-15 13:55:17 +0300

    Fix global style

--- src/components/pages/promptbook/images-box/index.tsx
+++ src/components/pages/promptbook/images-box/index.tsx
@@ -18,7 +18,7 @@ export default function ImagesBox({
   images: number[];
 }) {
   return (
-    <Slider {...settings}>
+    <Slider {...settings} className="mobileView">
       {images.map((item) => (
         <div key={item} className={styles.item}>
           <img
