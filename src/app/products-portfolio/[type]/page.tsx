48d952b94c58908b438577794440bdff1e5b3f01
Andrey Murashev <andreymurashev@gmail.com>
2026-01-21 20:53:45 +0300

    Fix TS

--- src/app/products-portfolio/[type]/page.tsx
+++ src/app/products-portfolio/[type]/page.tsx
@@ -1,4 +1,5 @@
 import PageEntry, { generateMetadata } from "../../[lang]/products-portfolio/[type]/page";
 
 export { generateMetadata };
+// eslint-disable-next-line @typescript-eslint/no-explicit-any react/display-name
 export default async (params: any) => <PageEntry {...params} />;
--- src/app/prompt-books/page.tsx
+++ src/app/prompt-books/page.tsx
@@ -1,4 +1,5 @@
 import PageEntry, { metadata } from "../[lang]/prompt-books/page";
 
 export { metadata };
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
 export default async (params: any) => <PageEntry {...params} />;
