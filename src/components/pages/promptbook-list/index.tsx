7855f5c69d52770c65668fa365771593c39da094
Andrey Murashev <andreymurashev@gmail.com>
2026-02-04 11:04:34 +0300

    Test scroll

--- src/components/pages/promptbook-list/index.tsx
+++ src/components/pages/promptbook-list/index.tsx
@@ -25,15 +25,16 @@ import { getPromptCategoryShortLabel, getPromptCategoryTag } from "@/constants/p
 
 export const settings = {
   dots: false,
-  // infinite: false,
+  infinite: false,
   lazyLoad: true,
   speed: 300,
   centerMode: false,
   slidesToShow: 3.3,
   slidesToScroll: 1,
+  nextArrow: null,
+  prevArrow: null,
   responsive: [
     {
-      arrows: true,
       breakpoint: 1024,
       settings: {
         slidesToShow: 3.3,
@@ -48,10 +49,9 @@ export const settings = {
       },
     },
     {
-      arrows: false,
       breakpoint: 480,
       settings: {
-        slidesToShow: 2,
+        slidesToShow: 2.3,
         slidesToScroll: 2,
       },
     },
