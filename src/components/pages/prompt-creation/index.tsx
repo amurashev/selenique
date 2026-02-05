b8fc35acda505b2d42f1149e23706592c2357443
Andrey Murashev <andreymurashev@gmail.com>
2026-02-05 15:14:38 +0300

    Fix TS

--- src/components/pages/prompt-creation/index.tsx
+++ src/components/pages/prompt-creation/index.tsx
@@ -34,13 +34,13 @@ export default function PromptCreationPage() {
     const copyTextEl = document.getElementById("prompt");
 
     if (copyTextEl) {
-      // eslint-disable @typescript-eslint/no-explicit-any
+      /* eslint-disable @typescript-eslint/no-explicit-any */
       // Select the text field
       (copyTextEl as any).select();
       (copyTextEl as any).setSelectionRange(0, 99999); // For mobile devices
       // Copy the text inside the text field
       navigator.clipboard.writeText((copyTextEl as any).value);
-      // eslint-enable @typescript-eslint/no-explicit-any
+      /* eslint-enable @typescript-eslint/no-explicit-any */
     }
   }
 
