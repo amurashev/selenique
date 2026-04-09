d4a047f1caf116959250af9e353dc898a1e3724e
Andrey Murashev <andreymurashev@gmail.com>
2026-04-09 11:56:51 +0300

    Update images for 50-old-money-ai-photo-prompts

--- src/content/promptbooks/index.ts
+++ src/content/promptbooks/index.ts
@@ -212,7 +212,7 @@ export const PROMTBOOKS: Record<string, PromptBookRaw> = {
     tags: ["fashion", "women", "lifestyle"],
     mainCategory: "lifestyle",
     type: "pack",
-    images: [1, 3, 4, 5, 6, 7, 8, 9, 10, 11],
+    images: [1, 4, 5, 6, 7, 10, 11],
   },
 
   // "50-old-money-ai-photo-prompts-vol2": {
