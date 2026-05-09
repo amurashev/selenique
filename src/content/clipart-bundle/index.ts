3b75807eca37548433b8b8845188f0f5e5af8b24
Andrey Murashev <andreymurashev@gmail.com>
2026-05-09 10:35:58 +0300

    Add some new bundles

--- src/content/clipart-bundle/index.ts
+++ src/content/clipart-bundle/index.ts
@@ -16,5 +16,26 @@ export const CLIPART_BUNDLES: Record<string, ClipartBundle> = {
     images: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
     title: "Watercolor Gender Reveal Clipart Bundle | Pink Blue Baby Shower Graphics",
   },
+  "graduation-class-of-2026": {
+    id: 2,
+    etsyID: 4470339750,
+    count: 50,
+    images: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
+    title: "Graduation Clipart Bundle, Class of 2026 PNG Graphics, Congrats Grad Party Decor",
+  },
+  "blush-gold-floral-clipart-bundle": {
+    id: 3,
+    etsyID: 4471314300,
+    count: 66,
+    images: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
+    title: "Blush Gold Floral Clipart Bundle, Watercolor Peonies, Roses, Frames",
+  },
+  "mystical-digital-stickers-witchy-clipart": {
+    id: 4,
+    etsyID: 4484561830,
+    count: 52,
+    images: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
+    title: "Mystical Celestial Clipart Bundle, Black Gold Witchy PNG Stickersr",
+  },
 }
 
