08505e1a339c3420a843b796f504d62b049be4ea
Andrey Murashev <andreymurashev@gmail.com>
2026-05-12 14:17:59 +0300

    Add isFree

--- src/constants/guides/index.ts
+++ src/constants/guides/index.ts
@@ -193,7 +193,7 @@ export const GUIDES: Record<string, GuideItem> = {
     locales: {
       en: {
         noLanding: true,
-        isBestseller: true,
+        // isBestseller: true,
         isFree: true,
         id: 71,
         gumroadSlug: "ai-photoshoot-guide",
