ed5ecd8573adcd37e2a835f05e5c693a4001c640
Andrey Murashev <andreymurashev@gmail.com>
2026-05-20 10:35:28 +0300

    Update cartoon-style-image-prompts

--- src/content/promptbooks/index.ts
+++ src/content/promptbooks/index.ts
@@ -493,7 +493,7 @@ export const PROMTBOOKS: Record<string, PromptBookRaw> = {
     type: "pack",
     fileType: "link",
     images: [1],
-    examples: [7, 8, 9, 10, 11, 12, 13],
+    examples: [2, 3, 4, 5, 6, 7, 8],
     og: true,
   },
   "studio-poses-photo-prompts": {
