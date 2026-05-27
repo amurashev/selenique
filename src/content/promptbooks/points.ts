85c265c65a502ba956beac11ec5041d706e1b594
Andrey Murashev <andreymurashev@gmail.com>
2026-05-27 15:18:37 +0300

    Change order

--- src/content/promptbooks/points.ts
+++ src/content/promptbooks/points.ts
@@ -1,23 +1,29 @@
 const PROMPTS_POINTS: Record<string, number> = {
   "custom-prompt-pack": 999,
-  "cartoon-style-image-prompts": 110,
-  "realistic-ai-influencer-prompts": 100,
-  "32-business-man-photo-prompts": 100,
+
+  "32-business-man-photo-prompts": 110,
+  "30-business-woman-photo-prompts-ai": 100,
   "35-cosmetic-product-photo-prompts": 100,
   "24-ai-maternity-photoshoot-prompts": 100,
-  "50-ideal-skin-midjourney-prompts-vol-1": 90,
-  "growing-prompt-library": 90,
+  "realistic-ai-influencer-prompts": 90,
 
   "women-fashion-editorial-prompts": 75,
   "men-fashion-editorial-prompts": 75,
-  "ai-avatar-free-prompts": 75,
+  "cartoon-style-image-prompts": 75,
+  "50-ideal-skin-midjourney-prompts-vol-1": 75,
+
   "cinematic-fantasy-photo": 50,
   "wedding-photoshoot": 50,
   "luxury-lifestyle-photo-prompts": 50,
+
+  "ai-avatar-free-prompts": 40,
+  "growing-prompt-library": 40,
   // "portrait-creation-toolkit": 20,
   // "mythical-character-photo": 10,
 
   "women-headshot-prompts-professional": -10,
+  "40-christmas-kids-photo-prompts-ai": -10,
+  "30-christmas-woman-photo-prompts": -10,
   // "22-business-man-headshot-prompts-ai": -10,
 }
 
