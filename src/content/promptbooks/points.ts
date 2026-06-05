23f7edc1ac69cf3db6b223b3b55847d4cbcdea65
Andrey Murashev <andreymurashev@gmail.com>
2026-06-05 12:29:39 +0300

    Update points

--- src/content/promptbooks/points.ts
+++ src/content/promptbooks/points.ts
@@ -1,29 +1,29 @@
 const PROMPTS_POINTS: Record<string, number> = {
   "custom-prompt-pack": 999,
 
-  "32-business-man-photo-prompts": 110,
-  "30-business-woman-photo-prompts-ai": 100,
-  "35-cosmetic-product-photo-prompts": 100,
-  "24-ai-maternity-photoshoot-prompts": 100,
-  "realistic-ai-influencer-prompts": 90,
+  "32-business-man-photo-prompts": 9,
+  "30-business-woman-photo-prompts-ai": 9,
+  "35-cosmetic-product-photo-prompts": 9,
+  "24-ai-maternity-photoshoot-prompts": 9,
 
-  "women-fashion-editorial-prompts": 75,
-  "men-fashion-editorial-prompts": 75,
-  "cartoon-style-image-prompts": 75,
-  "50-ideal-skin-midjourney-prompts-vol-1": 75,
+  "realistic-ai-influencer-prompts": 8,
+  "women-fashion-editorial-prompts": 8,
+  "men-fashion-editorial-prompts": 8,
+  "cartoon-style-image-prompts": 8,
+  "50-ideal-skin-midjourney-prompts-vol-1": 8,
 
-  "cinematic-fantasy-photo": 50,
-  "wedding-photoshoot": 50,
-  "luxury-lifestyle-photo-prompts": 50,
+  "cinematic-fantasy-photo": 7,
+  "wedding-photoshoot": 7,
+  "luxury-lifestyle-photo-prompts": 7,
 
-  "ai-avatar-free-prompts": 40,
-  "growing-prompt-library": 40,
+  "ai-avatar-free-prompts": 6,
+  "growing-prompt-library": 6,
   // "portrait-creation-toolkit": 20,
   // "mythical-character-photo": 10,
 
-  "women-headshot-prompts-professional": -10,
-  "40-christmas-kids-photo-prompts-ai": -10,
-  "30-christmas-woman-photo-prompts": -10,
+  "women-headshot-prompts-professional": -1,
+  "40-christmas-kids-photo-prompts-ai": -1,
+  "30-christmas-woman-photo-prompts": -1,
   // "22-business-man-headshot-prompts-ai": -10,
 }
 
