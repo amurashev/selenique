5f2a3def0d5e3fc2de3c2d69f4683dd23f91698b
Andrey Murashev <andreymurashev@gmail.com>
2026-06-12 17:19:38 +0300

    Add new packs

--- src/content/images-packs/index.ts
+++ src/content/images-packs/index.ts
@@ -2,11 +2,11 @@ export type ImagesPackRaw = {
   id: number;
   etsyID: number;
   gumroad?: string;
-  cmLink?: string
+  cmLink?: string;
   count: number;
   size: number[];
-  orientation: 'h' | 'v'
-  ar: "9:16" | "3:4" | "2:3"
+  orientation: "h" | "v";
+  ar: "9:16" | "3:4" | "2:3";
   images: number[];
 };
 
@@ -18,7 +18,7 @@ export type ImagesPack = ImagesPackRaw & {
   gumroadLink?: string;
   thumbImage: string;
   mainImage: string;
-  title: string
+  title: string;
   description: string;
   keywords: string;
   imagesContent: string[];
@@ -28,11 +28,11 @@ export const IMAGES_PACKS: Record<string, ImagesPackRaw> = {
   "faceless-stock-photos-lifestyle": {
     id: 1,
     etsyID: 4500430540,
-    cmLink: '292227597-330-Faceless-Stock-Photos',
+    cmLink: "292227597-330-Faceless-Stock-Photos",
     count: 330,
     size: [1536, 2752],
     ar: "9:16",
-    orientation: 'v',
+    orientation: "v",
     images: [4, 5, 6],
   },
   // "tarot-and-spiritual-stock-photos": {
@@ -43,13 +43,17 @@ export const IMAGES_PACKS: Record<string, ImagesPackRaw> = {
   //   title:
   //     "Tarot and Spiritual Stock Photos | Tarot Cards, Crystals, Moon Rituals, Witch Aesthetic & Spiritual Morning",
   // },
-  // "luxury-skincare-stock-photos-body": {
-  //   id: 3,
-  //   etsyID: 4350697370,
-  //   count: 120,
-  //   images: [],
-  //   title: "Luxury Skincare Stock Photos | Body & Wellness Photography",
-  // },
+  "luxury-skincare-stock-photos-body": {
+    id: 3,
+    etsyID: 4350697370,
+    cmLink: "291785476-130-Body-Aesthetic-Stock-Images",
+    gumroad: 'body-aesthetic-stock-images',
+    count: 130,
+    size: [4416, 5888],
+    ar: "3:4",
+    orientation: "v",
+    images: [5, 6, 7, 8],
+  },
   // "sport-aesthetic-stock-images": {
   //   id: 4,
   //   etsyID: 4355918918,
@@ -57,4 +61,14 @@ export const IMAGES_PACKS: Record<string, ImagesPackRaw> = {
   //   images: [],
   //   title: "Sport Stock Photos | Fitness Images",
   // },
+  "men-workout-stock-photos": {
+    id: 5,
+    etsyID: 4499672577,
+    cmLink: "292232220-100-Men-Workout-Stock-Photos-Bundle",
+    count: 100,
+    size: [2208, 2944],
+    ar: "3:4",
+    orientation: "v",
+    images: [7, 8, 9, 10, 11],
+  },
 };
